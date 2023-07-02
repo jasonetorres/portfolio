2b1d81b80e8051bfeb51fddcbe4d8d36eb3193f2
Jason Torres <jasontorres585@icloud.com>
2023-07-02 06:52:06 -0400

    portfolion build

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,74 @@
-import React from 'react'
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
-import Logo from '../../assets/logo (1).png';
+import Logo from "../../assets/logo (1).png";
 
-
+const Menu = () => {
+  return (
+  <>
+    <p>
+      <a href="#Home">Home</a>
+    </p>
+    <p>
+      <a href="#GPT3">What is GPT3?</a>
+    </p>
+    <p>
+      <a href="Poss">Open AI</a>
+    </p>
+    <p>
+      <a href="#Features">Case Studies</a>
+    </p>
+    <p>
+      <a href="#Blog">Library</a>
+    </p>
+  </>
+  );
+};
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className="T3_Navbar">
       <div className="T3_Navbar-links">
         <div className="T3_Navbar-links_logo">
-      <img src={Logo} alt="Logo" />
+          <img src={Logo} alt="Logo" />
         </div>
-
+        <div className="T3_Navbar-links-container">
+          <Menu />
+        </div>
+        <div className="T3_Navbar-sign">
+          <p>Sign In</p>
+          <button type="button">Sign Up</button>
+        </div>
+        <div className="T3_Navbar-menu">
+          {toggleMenu ? (
+            <RiCloseLine
+              color="#fff"
+              size={27}
+              onClick={() => setToggleMenu(false)}
+            />
+          ) : (
+            <RiMenu3Line
+              color="#fff"
+              size={27}
+              onClick={() => setToggleMenu(true)}
+            />
+          )}
+          {toggleMenu && (
+            <div className="T3_Navbar-menu_container scale-up-center">
+              <div className="T3_Navbar-menu_container-links">
+                <Menu />
+              </div>
+              <div className="T3_Navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
 export default Navbar;
